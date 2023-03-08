fec70ea069bd5a28ada542d52af16e2ad64a83c7
Annalisa Smith <113122282+annalisa7@users.noreply.github.com>
2023-03-08 17:13:49 +0000

    Update streamlit_app.py
    amended to add a error thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,16 @@ streamlit.dataframe(fruits_to_show)
 
 # New sectin to display fruityvice API 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.")
+  else:
+    fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #streamlit.text(fruityvice_response.json())
 
